6e820a67e2bdb338215261413fc717a3117eac7b
Marco Mettke <mrm@binyamen.ubuntu1604.wk.tb>
2018-05-15 10:14:49 +0200

    navbar runs

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+#Config MySQL
+app.config['MYSQL_HOST'] = '127.0.0.1'                  # here was a problem if you use localhost instead - always use 127.0.0.1
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'simple'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+#init MySQL
+mysql = MySQL(app)
 
 
 from myapp.home.view_home import mod
